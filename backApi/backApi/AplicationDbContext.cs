3d4750edc794f3ed30b30a40c1b53f41efc508e8
DARLINGRD01 <felizdarling49@gmail.com>
2023-09-14 08:17:19 -0400

    Commit inicial

---
+++ backApi/backApi/AplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using backApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace backApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCreditos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
